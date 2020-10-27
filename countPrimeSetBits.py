f506723b664f35c81b41a63088c8b20ec4fb1fbe
Rodrigo Milan Calegari <42324712+RodrigoMilan@users.noreply.github.com>
2020-10-27 16:19:49 -0300

    Create countPrimeSetBits.py

---
+++ countPrimeSetBits.py
@@ -0,0 +1,17 @@
+from collections import Counter
+class Solution:
+    def countPrimeSetBits(self, L: int, R: int) -> int:
+        primes= [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,53, 59, 61, 67, 71, 73, 79, 83, 89, 97 ,101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349]
+        
+        binaries = []
+        final = 0
+        
+        for x in range(L,R+1):
+            binaries.append(bin(x))
+        
+        for x in binaries:
+            count = Counter(x)
+            if count['1'] in primes:
+                final +=1
+                
+        return final
